69242b47a3380b55914fa043906bd3d8c86ef0fe
Rammy <kuttaineh@gmail.com>
2018-05-20 13:43:33 -0400

    Changed initial value of Pay from 1.00 to 0.01
    User(s) define their currency. For instance if Bitcoin Cash were chosen then according to https://qurrency.io/bch/usd/0.01 the initial base rate of pay, as of 2018-05-20 7:01 p.m. UTC, would be $12.59/hr.

--- payd.cpp
+++ payd.cpp
@@ -16,7 +16,7 @@ int printUsage(float);
 
 int main(int argc, char *argv[])
 {
-    float Pay = 1.00; // hourly
+    float Pay = 0.01; // hourly
     float Worth = 0.0000;
     int responsecode;
     
